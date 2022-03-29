75891ef912e66895c468436b3457e1c45513dd27
Eliran Shema <eliranshema@microsoft.com>
2022-03-29 13:42:22 +0300

    example

---
+++ ClassLibrary1/Classes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1 
+    {
+        public string Str { get; private set; }
+        public int Num { get; private set; }
+        public DateTime Date { get; private set; }
+        public Class1(string str, int num, DateTime date)
+        {
+            Str = str;
+            Num = num;
+            Date = date;
+        }
+    }
+
+    public class Class2
+    {
+        public Class2() // if removed everything works
+        { }
+        public Class2(string str, int num, DateTime date)
+        {
+            Str = str;
+            Num = num;
+            Date = date;
+        }
+        public string Str { get; private set; } // if remove the private, everything works
+        public int Num { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ test2.2.6/DynamicDataTest.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test2._2._6
+{
+    [TestClass]
+    public class DynamicDataTest
+    {
+        public static IEnumerable<object[]> TestCasesClass1()
+        {
+            yield return new object[] { new Class1("1",1,new DateTime(1, 1, 1))};
+        }
+
+        [DynamicData(nameof(TestCasesClass1), typeof(DynamicDataTest), DynamicDataSourceType.Method)]
+        [DataTestMethod]
+        public void TestDynamicDataClass1(Class1 item)
+        {
+            Assert.AreEqual(new DateTime(1, 1, 1), item.Date);
+            Assert.AreEqual(1, item.Num);
+            Assert.AreEqual("1", item.Str);
+        }
+
+        public static IEnumerable<object[]> TestCasesClass2()
+        {
+            yield return new object[] { new Class2("1", 1, new DateTime(1, 1, 1)) };
+        }
+
+        [DynamicData(nameof(TestCasesClass2), typeof(DynamicDataTest), DynamicDataSourceType.Method)]
+        [DataTestMethod]
+        public void TestDynamicDataClass2(Class2 item)
+        {
+            Assert.AreEqual(new DateTime(1, 1, 1), item.Date);
+            Assert.AreEqual(1, item.Num);
+            Assert.AreEqual("1", item.Str);
+        }
+    }
+}
